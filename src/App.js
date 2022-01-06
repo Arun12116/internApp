33174749a73953e4b187c0c7443a1dfca673214a
arun <yarunkumar136@gmail.com>
2022-01-06 09:54:00 +0530

    App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import WeatherApp from "./components/Weather/WeatherMain";
+
+function App() {
+
+  const ifConnetcted = window.navigator.onLine;
+
+  return (
+  <>
+        <div className={ifConnetcted ? "connect" : "notConneted"}></div>
+    <WeatherApp />
+  </>
+  );
+}
+
+export default App;
